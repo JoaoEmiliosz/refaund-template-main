fe066104522c835bea592837b40796dc3073248a
Joao Emilio <joaoemilio785@gmail.com>
2026-02-19 12:40:38 -0300

    li create

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+// Captura o evento de input para formatação de valor.
+amount.oninput = () => {
+    // Obtém o valor atual do input e remove todos os caracteres não numéricos.
+    let value = amount.value.replace(/\D/g, "")
+
+    // Transforma o valor em centavos.
+    value = Number(value) / 100
+
+    // Atualiza o valor do input com o valor formatado.
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formatação de valor para BRL.
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    // Retorna o valor formatado.
+    return value
+}
+
+// Captura o evento de submit do formulário.   
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar pag.
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes da nova despesa.
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+    // Chama a função que irá adicionar o ítem na lista.
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        // Cria o elemento de li (ítem) para adicionar o ítem na lista (ul).
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
+}
